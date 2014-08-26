f17d4d58c5047512b9c965670c1da6d7ae431f64
hikaMaeng <hika00@gmail.com>
2014-08-27 01:15:16 +0900

    scss파싱기능을 수행하도록 css함수 업그레이드

--- bsjs.0.5.js
+++ bsjs.0.5.js
@@ -768,7 +768,7 @@ fn( 'router', (function(){
 		bs.cls( 'Style', function( fn, clsfn, bs ){
 			var r = /-[a-z]/g, re = function(_0){return _0.charAt(1).toUpperCase();},
 			rn = /^([-]?[.0-9]+)([^-.0-9]*)$/g, rno = [], rne = function( $0, $1, $2){rno[0] = $1, rno[1] = $2;},
-			b = doc.body.style, pf = detect.stylePrefix, key, conf = {nopx:{}, key:{}, val:{}}, nopx = conf.nopx, keys = conf.key, vals = conf.val, t0;
+			b = doc.body.style, pf = detect.stylePrefix, key, conf = {nopx:{}, key:{}, val:{}}, nopx = conf.nopx, keys = conf.key, vals = conf.val, t0, t1;
 			clsfn.keys = keys,
 			clsfn.fn = function( type, key, val ){
 				conf[type][key] = val;
@@ -803,29 +803,111 @@ fn( 'router', (function(){
 				if( k = keys[t0 = k] ){if( typeof k == 'function' ) return k( this, s, v );}else if( !( k = key(t0) ) ) return 0;
 				return '@r@';
 			},
-			t0 = {keys:keys, key:key, rn:rn, rno:rno, rne:rne},
-			fn.g = comp( mk, {r:"this[k] === undefined ? rn.test( v = s[k] ) ? ( v.replace( rn, rne ), this.u[k] = rno[1], this[k] = parseFloat(rno[0]) ) : ( this.u[k] = '', this[k] = v ) : this[k]"}, t0 ), 
+			t0 = {keys:keys, key:key, rn:rn, rno:rno, rne:rne, nopx:nopx, trim:trim},
+			fn.g = comp( mk, {r:( t1 = ( function(){
+				this[k] === undefined ? 
+					( v = s[k] ) ? 
+						rn.test(v) ? 
+							( v.replace( rn, rne ), this.u[k] = rno[1], this[k] = parseFloat(rno[0]) ) : 
+							( this.u[k] = '', this[k] = v ) : 
+
+						( nopx[k] ? ( this.u[k] = '', this[k] = '' ) : ( this.u[k] = 'px', this[k] = 0 ) ) : 
+					this[k]
+			}).toString() ).substring( t1.indexOf('this'), t1.length - 1 )}, t0 ), 
 			fn.s = comp( mk, {r:'s[k] = ( this[k] = v ) + u[k], v'}, t0 );
 		} ),
-		bs.cls( 'Css', function( fn, clsfn, bs ){
-			var sheet = doc.createElement('style'), rule, ruleSet, ruleKey, idx, add, del, r = /^[0-9.-]+$/,
-			parser = function(data){
-				var t0, t1, t2, t3, c, i, j, k, l, v, m, sel, val;
-				t2 = [], t0 = data.split('}');
-				for( i = 0, j = t0.length ; i < j ; i++ ){
-					if( t0[i].replace( trim, '' ) ){
-						t1 = t0[i], sel = t1.substring( 0, m = t1.indexOf('{') ).replace( trim, '' ), val = t1.substr( m + 1 );
-						if( sel.indexOf('@') == -1 ){
-							c = bs.Css(sel), t1 = val.split(';'), t2.length = 0;
-							for (k = 0, l = t1.length; k < l ; k++) {
-								t3 = t1[k], t2[t2.length] = t3.substring( 0, m = t3.indexOf(':') ).replace( trim, '' ), t2[t2.length] = r.test( v = t3.substr( m + 1 ).replace( trim, '') ) ? parseFloat(v) : v;
-							};
-							c.S.apply( c, t2 );
-						}else if( sel.substr( 0, 9 ) == 'font-face' ) bs.Css( sel + ' ' + val.replace( trim, '' ) );
+		bs.fn( 'css', (function(trim){
+			var r = /^[0-9.-]+$/, VAR, MIX,
+			r0 = /\$[^;:]+[:][^;:]+;/g, f0 = function(v){
+				v = v.substring( 0, v.length - 1 ).split(':');
+				VAR[v[0]] = v[1];
+				return '';
+			},
+			r1 = /[@]mixin [^@{]+[{][^@{()}]+[}]/g, f1 = function(v){
+				var n, arg = '', b, i;
+				v = v.substr(7).split('{'), n = v[0].replace( trim, '' );
+				if( ( i = n.indexOf('(') ) > -1 ) arg = n.substring( i + 1, n.lastIndexOf(')') ).replace( trim, '' ), n = n.substring( 0, i ).replace( trim, '' );
+				b = v[1], b = b.substring( 0, b.lastIndexOf('}') ),
+				MIX[n] = new Function( 'b,arg,trim', ( i = (function(){
+					var r = [], j = arg.length, i = j;
+					while( i-- ) r[i] = new RegExp( '[$]' + arg[i].replace( trim, '' ).substr(1), 'g' );
+					return function(){
+						var i = j;
+						while(i--) b = b.replace( r[i], arguments[i] ? arguments[i].replace( trim, '' ) : '' );
+						return b;
+					};
+				}).toString(), i.substring( i.indexOf('var') - 1, i.lastIndexOf('}') ) ) )( b, bs.trim(arg.split(',')), trim );
+				return '';
+			},
+			wrapSel = [], wrapBody = [], rc = /[{]/g,
+			parser = function( v, V, M ){
+				var arg = [], t0, v, i, j, k, l, m, n, o, p, w0, w1, w2, w3, w4, w5, w6, sel, val;
+				VAR = V || {}, MIX = M || {}, v = v.replace( r1, f1 ).replace( r0, f0 );
+				for( v = v.split('}'), i = 0, j = v.length ; i < j ; i++ ){
+					if( t0 = v[i].replace( trim, '' ) ){
+						sel = t0.substring( 0, k = t0.indexOf('{') ).replace( trim, '' ), val = t0.substr( k + 1 );
+						if( val.indexOf('{') > -1 ){
+							for( wrapSel.length = wrapBody.length = 0, wrapSel[0] = sel, w4 = k = m = n = 0, l = val.match(rc).length ; k < l ; k++ ){
+								n = val.indexOf( '{', m );
+								w0 = val.substring( m, n );
+								w1 = w0.lastIndexOf(';') + 1;
+								w2 = w0.substr( w1 ).replace( trim, '' );
+								w3 = w0.substring( 0, w1 ).replace( trim, '' ) + v[i + l - k].replace( trim, '' );
+								if( w2.charAt( w2.length - 1 ) == ':' ){
+									w4 = w2.substring( 0, w2.length - 1 ) + '-',
+									wrapBody[wrapBody.length] = w3;
+								}else{
+									wrapSel[wrapSel.length] = w2;
+									if( w4 ){
+										for( w3 = w3.split(';'), w5 = wrapBody.length - 1, o = 0, p = w3.length ; o < p ; o++ ) if( w6 = w3[o].replace( trim, '' ) ) wrapBody[w5] += w4 + w6 + ';';
+										w4 = 0;
+									}else wrapBody[wrapBody.length] = w3;
+								}
+								m = n + 1;
+							}
+							w3 = val.substr(m).replace( trim, '' );
+							if( w4 ){
+								for( w3 = w3.split(';'), w5 = wrapBody.length - 1, o = 0, p = w3.length ; o < p ; o++ ) if( w6 = w3[o].replace( trim, '' ) ) wrapBody[w5] += w4 + w6 + ';';
+								w4 = 0;
+							}else wrapBody[wrapBody.length] = w3;
+							for( k = 0, l = wrapSel.length ; k < l ; k++ ){
+								css( wrapSel[k], wrapBody.slice( 0, k + 1 ).join('') );
+							}
+							i += l;
+						}else css( sel, val );
 					}
 				}
+			},
+			carg = [], css = function( sel, v ){
+				var c, t0, i, j, k, l;
+				if( sel.indexOf('@') == -1 ){
+					for( c = bs.Css(sel), v = v.split(';'), carg.length = 0, i = 0, j = v.length; i < j ; i++ ){
+						if( t0 = v[i].replace( trim, '' ) ){
+							if( t0.indexOf('@include') === 0 ){
+								t0 = ( k = t0.indexOf('(') ) > -1 ? 
+									MIX[t0.substring( 8, k ).replace( trim, '' )].apply( null, t0.substring( k + 1, t0.lastIndexOf(')') ).split(',') ) :
+									MIX[t0.substr(8).replace( trim, '' )]();
+								for( t0 = t0.split(';'), k = 0, l = t0.length ; k < l ; k++ ) add( carg, t0[k] );
+							}else if( t0.indexOf('@extend') === 0 ){
+							}else add( carg, t0 );
+						}
+					};
+					c.S.apply( c, carg );
+				}else if( sel.substr( 0, 9 ) == 'font-face' ) bs.Css( sel + ' ' + val.replace( trim, '' ) );
+			},
+			add = function( arg, v ){
+				var i, j;
+				if( v = v.replace( trim, '' ) ){
+					arg[arg.length] = v.substring( 0, i = v.indexOf(':') ).replace( trim, '' ), 
+					v = v.substr( i + 1 ).replace( trim, ''),
+					v = VAR[v] || v,
+					arg[arg.length] = r.test(v) ? parseFloat(v) : v;
+				}
 			};
-			bs.fn( 'css', function(v){v.substr( v.length - 4 ) == '.css' ? bs.get( parser, v ) : parser(v);} );
+			return function(v){v.substr( v.length - 4 ) == '.css' ? bs.get( parser, v ) : parser(v);};
+		})(trim) ),
+		bs.cls( 'Css', function( fn, clsfn, bs ){
+			var sheet = doc.createElement('style'), rule, ruleSet, ruleKey, idx, add, del;
 			doc.getElementsByTagName('head')[0].appendChild(sheet),
 			sheet = sheet.styleSheet || sheet.sheet, ruleSet = sheet.cssRules || sheet.rules,
 			ruleKey = {
