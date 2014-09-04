3d62c18aeae3c5f9a87093cf4ff300d49861c09b
hikaMaeng <hika00@gmail.com>
2014-09-04 16:02:48 +0900

    sass에서 css로 복귀

--- bsjs.0.6.js
+++ bsjs.0.6.js
@@ -66,7 +66,6 @@ detectWindow = function( W, detect ){
 			}
 			return 0;
 		})()
-		
 	} ) if( t0.hasOwnProperty(i) ) detect[i] = t0[i];
 	return detect;
 },
@@ -980,179 +979,25 @@ fn( 'router', (function(){
 				return this;
 			};
 		} ),
-		fn( 'css', (function( trim, css ){
-			var rSel = /[};][^};]+$/g, rParent = /[&]/g,
-			VAR = {}, rVAL = /\$[^;:]+[:][^;:]+;/g, fVAL = function(v){return v = v.substring( 0, v.length - 1 ).split(':'), VAR[v[0]] = pVal(v[1]), '';},
-			MIX = {}, rMIX = /[@]mixin [^@{]+[{][^}]+[}]/g, fMIX = function(v){
-				var n, arg = '', b, i;
-				v = v.substr(7).split('{'), n = v[0].replace( trim, '' );
-				if( ( i = n.indexOf('(') ) > -1 ) arg = n.substring( i + 1, n.lastIndexOf(')') ).replace( trim, '' ), n = n.substring( 0, i ).replace( trim, '' );
-				b = v[1], b = b.substring( 0, b.lastIndexOf('}') ),
-				MIX[n] = new Function( 'b,arg,trim', ( i = (function(){
-					var r = [], j = arg.length, i = j;
-					while( i-- ) r[i] = new RegExp( '[$]' + arg[i].replace( trim, '' ).substr(1), 'g' );
-					return function(){
-						var i = j;
-						while(i--) b = b.replace( r[i], arguments[i] ? arguments[i].replace( trim, '' ) : '' );
-						return b;
-					};
-				} ).toString(), i.substring( i.indexOf('var') - 1, i.lastIndexOf('}') ) ) )( b, arg.split(','), trim );
-				return '';
-			},
-			FUNC = {},
-			imports = [], rImport = /[@]import ['"]?([^'"]+)['"][;]/g, fImport = function( g, v ){
-				return imports[g = imports.length] = v.indexOf('.') == -1 ? v + '.css' : v, '@I' + g + 'I@';
-			},
-			extend, rExtend = /[@]extend (.+)[;]/g, fExtend = function( g, v ){return extend[v] || '';},
-			placeholder = {},
-			pEx = (function(){
-				var rnum = /^[-]?[0-9.]+$/, rstr = /^(["][^"]*["]|['][^']*['])$/, rkey = /^[a-zA-z0-9 ]+$/,
-				rvar = /^[$]\S+$/, runit = /^([-]?[0-9.]+)([^-0-9.]+)$/, rcolor = /(.)/g,
-				ra = /\[[^\[\]]*\]/g, rf = /[a-zA-z$_][a-zA-z0-9$_]*[ ]*[(][^)]*[)]/g, rp = /[(][^)]*[)]/,
-				rarr = /^[@]A[0-9]+[:][0-9]+[@]$/, rfunc = /^[@]F[0-9]+[:][0-9]+[@]$/, rparen = /^[@]P[0-9]+[:][0-9]+[@]$/,
-				val = {'true':true,'false':false,'null':null,'undefined':undefined},
-				op = {'+':1,'-':1,'*':2,'/':2},
-				opf = {'+':function(a,b){return a+b;},'-':function(a,b){return a-b;},'*':function(a,b){return a*b;},'/':function(a,b){return a/b;}},
-				Value = (function(){
-					var v = function(){
-						var i = 0, j = arguments.length;
-						while( i < j ) this[arguments[i++]] = arguments[i++];
-					}, fn = v.prototype;
-					fn.value = 0, fn.string = '', fn.toString = fn.valueOf = function(){return this.value;};
-					return v;
-				})(),
-				ex = function( v, f, p, a ){
-					var depth, ff, fp, fa, arg, t0, t1, i, j, k;
-					if( i = val[v = v.replace( trim, '' )]) return i;
-					if( v.charAt(0) == '#' ){
-						if( v.length == 4 ) v = '#' + v.substr(1).replace( rcolor, '$1$1');
-						return new Value( 'value', v, 'r', parseInt( '0x' + v.substr( 1, 2 ) ), 'g', parseInt( '0x' + v.substr( 3, 2 ) ), 'b', parseInt( '0x' + v.substr( 5, 2 ) ) );
+		fn( 'css', (function(trim){
+			var r = /^[-]?[0-9.]+$/, s = bs.Style(), c = bs.Css('.BSCSS000').style,
+			css = function(v){return c.cssText = '', s.S( c, v, 0 ), c.cssText;},
+			arg = [], parser = function(v){
+				var w = '', s, b, t0, t1, i, j, k, l, m;
+				for( v = v.split('}'), i = 0, j = v.length ; i < j ; i++ )
+					if( t0 = v[i].replace( trim, '' ) ){
+						s = t0.substring( 0, k = t0.indexOf('{') ).replace( trim, '' ), b = t0.substr( k + 1 );
+						if( s.indexOf('@') == -1 ){
+							for( t0 = b.split(';'), arg.length = k = 0, l = t0.length ; k < l ; k++ )
+								t1 = t0[k], arg[arg.length] = t1.substring( 0, m = t1.indexOf(':') ).replace( trim, '' ), arg[arg.length] = r.test( t1 = t1.substr( m + 1 ).replace( trim, '') ) ? parseFloat(t1) : t1;
+							w += s + '{' + css(arg) + '}\n';
+						}else if( s.substr( 0, 9 ) == 'font-face' ) bs.Css( s + ' ' + b.replace( trim, '' ) );
+						else w += t0 + '}\n';
 					}
-					if( rvar.test(v) ) return VAR[v];
-					if( rnum.test(v) ) return parseFloat(v);
-					if( runit.test(v) ) return new Value( 'value', v, 'unit', ( t1 = v.match(runit), t1[2] ), 'v', parseFloat(t1[1]) );
-					if( rstr.test(v) ) return v.substring( 1, v.length - 1 );
-					if( rkey.test(v) ) return v;
-					if( rarr.test(v) ){
-						for( t0 = a[v.substring( 2, i = v.indexOf(':') )][v.substring( i + 1, v.length - 1 )].split(','), i = 0, j = t0.length ; i < j ; i++ ) t0[i] = ex( t0[i], f, p, a );
-						return t0;
-					}
-					if( rfunc.test(v) ){
-						v = f[v.substring( 2, i = v.indexOf(':') )][v.substring( i + 1, v.length - 1 )], t0 = FUNC[k = v.substring( 0, i = v.indexOf('(') )];
-						for( t1 = v.substring( i + 1, v.length - 1 ).split(','), i = 0, j = t1.length ; i < j ; i++ ) t1[i] = ex( t1[i], f, p, a );
-						return t0 ? t0.apply( t0, t1 ) : k + '(' + t1.join(',') + ')';
-					}
-					if( rparen.test(v) ) return ex( p[v.substring( 2, i = v.indexOf(':') )][v.substring( i + 1, v.length - 1 )], f, p );
-					if( v.indexOf('(') > -1 ){
-						f = [], ff = function(v){
-							var t0 = f[depth] || ( f[depth] = [] ), i = t0.length;
-							return t0[i] = v, '@F' + depth + ':' + i + '@';
-						},
-						p = [], fp = function(v){
-							var t0 = p[depth] || ( p[depth] = [] ), i = t0.length;
-							return t0[i] = v.substring( 1, v.length - 1 ).replace( trim, '' ), '@P' + depth + ':' + i + '@';
-						},
-						depth = 0;while( rf.test(v) ) v = v.replace( rf, ff ), depth++;
-						depth = 0;while( rp.test(v) ) v = v.replace( rp, fp ), depth++;
-					}
-					if( v.indexOf('[') > -1 ){
-						a = [], fa = function(v){
-							var t0 = a[depth] || ( a[depth] = [] ), i = t0.length;
-							return t0[i] = v.substring( 1, v.length - 1 ).replace( trim, '' ), '@A' + depth + ':' + i + '@';
-						},
-						depth = 0;while( ra.test(v) ) v = v.replace( ra, fa ), depth++;
-					}
-					for( arg = [], i = k = 0, j = v.length ; i < j ; i++ ) if( op[t0 = v.charAt(i)] ) arg[arg.length] = ex( v.substring( k, i ), f, p, a ), arg[arg.length] = t0, k = i + 1;
-					arg[arg.length] = ex( v.substr(k), f, p, a );
-					i = 3;
-					while( i-- > 1 ){
-						j = 1;
-						while( j < arg.length ) if( op[arg[j]] == i ) arg.splice( j - 1, 3, opf[arg[j]]( arg[j - 1], arg[j + 1] ) ); else j += 2;
-					}
-					return arg[0];
-				};
-				return ex;
-			})(),
-			pAdd = function( v, arg, bodys ){
-				var i, j;
-				if( v.indexOf('@include') === 0 ){
-					v = ( i = v.indexOf('(') ) > -1 ? 
-						MIX[v.substring( 8, i ).replace( trim, '' )].apply( null, v.substring( i + 1, v.lastIndexOf(')') ).split(',') ) :
-						MIX[v.substr(8).replace( trim, '' )]();
-					for( v = v.split(';'), i = 0, j = v.length ; i < j ; i++ ) pAdd( v[i], arg, bodys );
-				}else if( v = v.replace( trim, '' ) ){
-					arg[arg.length] = v.substring( 0, i = v.indexOf(':') ).replace( trim, '' ),
-					arg[arg.length] = pEx(v.substr( i + 1 ).replace( trim, ''));
-				}
-			},
-			pData = function( v, depth, sels, bodys ){
-				var i, j;
-				for( v = v.replace( trim, '' ).split('}'), i = 0, j = v.length ; i < j ; i++ ){
-					if( depth && v[i] ) bodys[sels[depth - 1]] += v[i].replace( trim, '' );
-					depth--;
-				}
-				return depth + 1;
-			},
-			parser = function( v ){
-				var imported, sels, bodys, parent, sorts, depth, self, c, t0, t1, i, j, k, l, w0, w1, w2;
-				imports.length = 0,
-				v = v.replace( rImport, fImport ).replace( rMIX, fMIX ).replace( rVAL, fVAL );
-				if( imports.length ) return ( imported = function(data){
-					data && ( v = v.replace( '@I' + imports.length + 'I@', data ) ), imports.length ? bs.get( imported, imports.pop() ) : parser(v);
-				})();
-				sels = [], bodys = {}, parent = {}, sorts = [];
-				for( k in placeholder ) bodys[k] = placeholder[k];
-				depth = i = j = 0;
-				while( ( j = v.indexOf( '{' , j ) ) > -1 ){
-					w0 = v.substring( i, j ).replace( trim, '' ),
-					bodys[w2 = ( w1 = w0.match(rSel) ) ? w1[0].substr(1).replace( trim, '' ) : w0] = '',
-					parent[w2] = sels.slice( 0, depth = pData( w0.substring( 0, w0.indexOf(w2) ), depth, sels, bodys ) ),
-					sels[depth] = w2, i = ++j, depth++;
-				}
-				pData( v.substring(i), depth, sels, bodys );
-				for( k in parent ){
-					t0 = parent[k];
-					if( i = t0.length ) i--, t1 = sorts[i] || ( sorts[i] = [] ), t1[t1.length] = [k, t0[i]];
-				}
-				for( i = 0, j = sorts.length ; i < j ; i++ )
-					for( t0 = sorts[i], k = 0, l = t0.length ; k < l ; k++ ){
-						self = t0[k][0], parent = t0[k][1], bodys[t1 = self.replace( rParent, parent )] = bodys[parent] + bodys[self];
-						if( t1 != self ) delete bodys[self];
-					}
-				extend = bodys;
-				for( k in bodys ) bodys[k] = bodys[k].replace( rExtend, fExtend );
-				w0 = '';
-				for( k in bodys ){
-					if( k.indexOf('@') == -1 ){
-						if( k.charAt(0) == '%' ){
-							placeholder[k] = bodys[k];
-							continue;
-						}
-						for( v = bodys[k].split(';'), sels.length = i = 0, j = v.length; i < j ; i++ ) if( t0 = v[i].replace( trim, '' ) ) pAdd( t0, sels, bodys );
-						if( isDebug ) console.log( k, '{', sels.join(','), '}' );
-						w0 += k + '{' + css(sels) + '}\n';
-					}else w0 += k + '{' + bodys[k] + '}';
-				}
-				document.getElementsByTagName('head')[0].appendChild( w1 = document.createElement('style') ),
-				w1['styleSheet'] ? ( w1['styleSheet'].cssText = w0 ) : ( w1.innerHTML = w0 );
-			},
-			f = function( v, val, fn ){
-				var k;
-				if( val ) for( k in val ) VAR[k] = val[k];
-				if( fn ) for( k in fn ) FUNC[k] = fn[k];
-				v.substr( v.length - 4 ) == '.css' ? bs.get( parser, v ) : parser(v);
+				doc.getElementsByTagName('head')[0].appendChild( t0 = doc.createElement('style') ), t0['styleSheet'] ? ( t0['styleSheet'].cssText = w ) : ( t0.innerHTML = w );
 			};
-			return f.fn = (function(){
-				var t = {mixin:MIX, 'var':VAR, 'function':FUNC};
-				return function( type ){
-					var t0, i = 1, j = arguments.length;
-					if( t0 = t[type] ) while( i < j ) t0[arguments[i++]] = arguments[i++];
-				};
-			})(), f;
-		})( trim, (function(){
-			var s = bs.Style(), c = bs.Css('.BSCSS000').style;
-			return function(v){return c.cssText = '', s.S( c, v, 0 ), c.cssText;};
-		})() ) ),
+			return function(v){v.substr( v.length - 4 ) == '.css' ? bs.get( parser, v ) : parser(v);};
+		})(trim) ),
 		bs.cls( 'Dom', function( fn, clsfn, bs ){
 			var ev = bs.ev, del, dom, domData, first = {}, childS;
 			clsfn.data = domData = (function(){
@@ -1816,38 +1661,6 @@ fn( 'router', (function(){
 		fn = bs.header,
 		fn( 'Cache-Control', 'no-cache' ),
 		fn( 'Content-Type', function(type){return ( type == 'GET' ? 'text/plain' : 'application/x-www-form-urlencoded' ) + '; charset=UTF-8';} );
-		css:
-		bs.css.fn( 'function',
-			'rgb', function( r, g, b ){
-				return new Value( 'r', r, 'g', g, 'b', b, 'string', 'rgb(' + r + ',' + g + ',' + b + ')' );
-			},
-			'rgba', function( r, g, b, a ){
-				return new Value( 'r', r = +r, 'g', g = +g, 'b', b = +b, 'a', a = +a, 'string', 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')' );
-			},
-			'hsl', t0 = (function(){
-				var f = function( p, q, t ){
-					if(t < 0) t += 1; else if(t > 1) t -= 1;
-					return t < 1 / 6 ? p + (q - p) * 6 * t : t < 1 / 2 ? q : t < 2 / 3 ? p + (q - p) * ( 2 / 3 - t ) * 6 : p;
-				}
-				return function( h, s, l ){
-					var r, g, b, p, q;
-					if( h > 360 ) h -= parseInt( h / 360 ) * 360;
-					h /= 360;
-					if( s == 0 ) r = g = b = l;
-					else p = 2 * l - ( q = l < .5 ? l * ( 1 + s ) : l + s - l * s ), r = f( p, q, h + 1 / 3 ), g = f( p, q, h ), b = f( p, q, h - 1 / 3 );
-					return this[arguments.length > 3 ? 'rgba' : 'rgb']( Math.round( r * 255 ), Math.round( g * 255 ), Math.round( b * 255 ), arguments[3] );
-				};
-			})(),
-			'hsla', t0,
-			'mix', function( c0, c1 ){
-				var w0 = 1, w1 = 1, r, g, b;
-				if( arguments.length > 2 ) w0 = arguments[2], w1 = 1 - w0;
-				r = parseInt( ( c0.r * w0 + c1.r * w1 ) * .5 ),
-				g = parseInt( ( c0.g * w0 + c1.g * w1 ) * .5 ),
-				b = parseInt( ( c0.b * w0 + c1.b * w1 ) * .5 );
-				return c0.a !== undefined && c1.a !== undefined ? this.rgba( r, g, b, ( c0.a * w0 + c1.a * w1 ) * .5 ) : this.rgb( r, g, b );
-			}
-		);
 		STYLE:
 		fn = bs.Style.fn;
 		(function(trim){
@@ -1886,8 +1699,7 @@ fn( 'router', (function(){
 		(function(trim){
 			var k, x, y, t = detect.text, ds0 = {}, del = bs.Dom.del, html = bs.Dom.html, dom = bs.Dom.dom, tagNodes = bs.Dom.tagNodes, ev = bs.ev.dom, t0, t1, t2;
 			for( k in t0 = {
-				'event':2,
-				'this':3,
+				'event':2, 'this':3,
 				isCapture:function(d){
 					var t0 = bs.Dom.data(d), t1 = t0.BSdomE;
 					return arguments.length == 1 ? t1 ? t1.isCapture : 0 : ( t1 || ( t0.BSdomE = ev(d) ) ).isCapture = arguments[1];
@@ -1925,12 +1737,7 @@ fn( 'router', (function(){
 						return e.eventType = v, e;
 					} : function(){},
 					fire = doc.dispatchEvent ? function( d, e ){d.dispatchEvent(e);} : doc.fireEvent ? function( d, e ){d.fireEvent( 'on' + e.eventType, e );} : 0;
-					return function( d, k, v ){
-						if( k = mk(v) ){
-							k.eventName = v;
-							fire( d, k );
-						}
-					};
+					return function( d, k, v ){if( k = mk(v) ) k.eventName = v, fire( d, k );};
 				})()
 			} )if( t0.hasOwnProperty(k) ) fn( 'key', k, t0[k] );
 			fn( 'key', 'before', comp( t0 = function( d, k, v ){
