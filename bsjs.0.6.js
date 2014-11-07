aafc3f1d8e8ef25d2df9902d2c2c2ed3b8566b90
hikaMaeng <hika00@gmail.com>
2014-11-07 16:30:09 +0900

    mvc변경

--- bsjs.0.6.js
+++ bsjs.0.6.js
@@ -629,34 +629,43 @@ HTML5:
 })();
 MVC:
 bs.obj( 'MVC', (function(){
-	var modelView = {}, view = {}, C = {}, M = {}, _all = {};
+	var trigger = {}, view = {}, C = {}, M = {}, _all = {}, _alla = [_all], a = [], 
+	m = function(){
+		var t0, i = 0, j = arguments.length, k, v, m, n;
+		while( i < j ){
+			k = arguments[i++];
+			if( i == j ) return M[k];
+			M[k] = v = arguments[i++];
+			if( t0 = trigger[k] )
+				if( t0[0] == _all || t0[0] === v ){
+					m = 1, n = t0.length, a.length = 0, a[0] = v;
+					while( m < n ) a[a.length] = M[t0[m++]];
+					view[k].apply( null, a );
+				}
+		}
+		return v;
+	};
 	return {
-		m:function(){
-			var i = 0, j = arguments.length, k, v;
-			while( i < j ){
-				k = arguments[i++];
-				if( i == j ) return M[k];
-				M[k] = v = arguments[i++];
-				if( k in modelView && ( modelView[k] == _all || modelView[k] === v ) ) view[k](v);
-			}
-			return v;
-		},
-		v:function( k ){
-			var i = arguments.length;
-			if( i == 1 ) return view[k];
-			view[k] = arguments[1];
-			if( i == 3 ) modelView[k] = arguments[2];
-		},
-		c:function( v, arg ){
-			var t0;
-			if( this.nodeType ){
-				t0 = bs.Dom.pool('@BSMVC', this );
-				v = t0.S('*bsMVC:route'), arg = t0.S('*bsMVC:param');
-			}
-			C[v](arg);
-		},
 		all:function(){return _all},
-		route:function( k, v ){C[k] = v}
+		router:function( k, arg ){
+			var t0;
+			if( this.nodeType ) t0 = bs.Dom.pool('@BSMVC', this ), k = t0.S('*bsMVC:route'), arg = t0.S('*bsMVC:param');
+			C[k]( m, arg );
+		},
+		toggle:function(k){return ( M[k] = M[k] ? 0 : 1 ) ? 0 : 1;},
+		c:function(){
+			var i = 0, j = arguments.length;
+			while( i < j ) C[arguments[i++]] = arguments[i++];
+		},
+		v:function( k, v ){
+			var t0, i, j = arguments.length;
+			view[k] = arguments[1];
+			if( j > 2 ){
+				t0 = [], i = 2;
+				while( i < j ) t0[t0.length] = arguments[i++];
+				trigger[k] = t0;
+			}else trigger[k] = _alla;
+		}
 	};
 })() );
 PLUGIN:
